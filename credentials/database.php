7f9fe2ecdb7bd0bb1c84fd353ce8afef210c2e81
Framcesco Sardaro <francesco.sardaro99@gmail.com>
2020-11-21 23:52:32 +0100

    Aggiungi un file che contiene credenziali e altre info del DB

---
+++ credentials/database.php
@@ -0,0 +1,5 @@
+<?php
+    define('BP4Y_DB_HOST', getenv('BP4Y_DB_HOST'));
+    define('BP4Y_DB_NAME', getenv('BP4Y_DB_NAME'));
+    define('BP4Y_DB_USERNAME', getenv('BP4Y_DB_USERNAME'));
+    define('BP4Y_DB_PASSWORD', getenv('BP4Y_DB_PASSWORD'));
